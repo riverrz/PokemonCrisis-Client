4eab994d2dac1cc362eecb4b1389faf2587e0b4d
Aakash Singh <aakashsingh1999@gmail.com>
2019-01-19 17:50:20 +0530

    Add files via upload

---
+++ js/active-pokemon.js
@@ -0,0 +1,29 @@
+function mypoke1() {
+  var popup = document.getElementById("active-poke1");
+  popup.classList.toggle("show");
+}
+
+function mypoke2() {
+  var popup = document.getElementById("active-poke2");
+  popup.classList.toggle("show");
+}
+
+function mypoke3() {
+  var popup = document.getElementById("active-poke3");
+  popup.classList.toggle("show");
+}
+
+function mypoke4() {
+  var popup = document.getElementById("active-poke4");
+  popup.classList.toggle("show");
+}
+
+function mypoke5() {
+  var popup = document.getElementById("active-poke5");
+  popup.classList.toggle("show");
+}
+
+function mypoke6() {
+  var popup = document.getElementById("active-poke6");
+  popup.classList.toggle("show");
+}
